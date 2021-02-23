0b4f34a9e76feb217fe40bf836526e8091c536fa
Yves Guillemot <yc.guillemot@wanadoo.fr>
2021-02-23 14:09:00 +0100

    Fix signal with complex signature. Add some tests.

--- src/runtests.sh
+++ src/runtests.sh
@@ -3,10 +3,12 @@
 export LD_LIBRARY_PATH=.
 export RAKULIB=.
 
+raku tests/05-QApplication.t
 raku tests/10-QEvent.t
 raku tests/20-QPoint.t
 raku tests/30-QPointF.t
 raku tests/40-connect.t
+raku tests/41-disconnect.t
 raku tests/50-QPushButton.t
 raku tests/60-QBoxLayout.t
 
